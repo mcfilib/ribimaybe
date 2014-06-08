54853321c12722435ac510390706d8490a070a72
unsymbol <hello@philipcunningham.org>
2014-06-08 11:43:03 +0100

    add initial implementation.

--- lib/ribimaybe.rb
+++ lib/ribimaybe.rb
@@ -1,5 +1,83 @@
 require "ribimaybe/version"
 
 module Ribimaybe
-  # Your code goes here...
+  module Maybe
+    class Nothing
+      def self.maybe(default, &fn)
+        default
+      end
+
+      def self.to_s
+        "Nothing"
+      end
+    end
+
+    class Just
+      def initialize(value)
+        @value = value
+      end
+
+      def maybe(default, &fn)
+        fn.(@value)
+      end
+
+      def to_s
+        "Just(#{@value.inspect})"
+      end
+    end
+
+    def Maybe(value)
+      value ? Just.new(value) : Nothing
+    end
+  end
+
+  module Maybe
+    class Nothing
+      def self.map(&fn)
+        self
+      end
+    end
+
+    class Just
+      def map(&fn)
+        Just.new(fn.(@value))
+      end
+    end
+  end
+
+  module Maybe
+    class Nothing
+      def self.apply(value)
+        self
+      end
+    end
+
+    class Just
+      def apply(value)
+        value.map { |v| @value.(v) }
+      end
+    end
+
+    def pure(value)
+      Maybe(value)
+    end
+  end
+
+  module Maybe
+    class Nothing
+      def self.bind(&fn)
+        self
+      end
+    end
+
+    class Just
+      def bind(&fn)
+        fn.(@value)
+      end
+    end
+
+    def rturn(value)
+      pure(value)
+    end
+  end
 end
