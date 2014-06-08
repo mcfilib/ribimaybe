bf8297c3a3d96ce70b8adbafdb2f2e1ed312126c
unsymbol <hello@philipcunningham.org>
2014-06-08 12:11:04 +0100

    tidy up specs.

--- spec/functor_spec.rb
+++ spec/functor_spec.rb
@@ -3,27 +3,20 @@ include Ribimaybe::Maybe
 
 describe "Functor Instance" do
 
-  let(:nothing) do
-    Nothing
-  end
-
-  let(:something) do
-    Just.new(41)
-  end
-
   describe "#map" do
     context "when i have nothing" do
       it "should give me back nothing" do
-        result = nothing.map { |x| x + 1 }
-        expect(result).to eq(Nothing)
+        result = Nothing.map { |x| x + 1 }
+        expect(result).to eq Nothing
       end
     end
 
     context "when i have something" do
       it "should apply the fn and we-wrap the value" do
-        result = something.map { |x| x + 1 }
-        expect(result).to eq(Just.new(42))
+        result = Just.new(41).map { |x| x + 1 }
+        expect(result).to eq Just.new(42)
       end
     end
   end
+
 end
