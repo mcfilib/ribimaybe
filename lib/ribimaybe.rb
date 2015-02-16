171cdfc921b3733629e92b486726700f811771b8
Philip Cunningham <philip@irisconnect.co.uk>
2015-02-16 21:38:37 +0000

    add alias for >= to bind

--- lib/ribimaybe.rb
+++ lib/ribimaybe.rb
@@ -165,9 +165,11 @@ module Ribimaybe
       # end # => Just(2)
       #
       Contract Proc => Or[Nothing, Just]
-      def bind(&fn)
-        fn.curry.(@value)
+      def bind(fn = nil, &block)
+        (fn || block).curry.(@value)
       end
+
+      alias_method :>=, :bind
     end
 
     # Converts nil to Nothing or lifts value into a Just. Accepts a optional
